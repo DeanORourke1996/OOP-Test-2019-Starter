a40a02824f82cf395ec703da9c943e71ad1240a6
Dean O'Rourke <c17737111@mydit.ie>
2019-04-10 16:47:25 +0100

    added colours contructor and overload method

--- src/ie/tudublin/Colours.java
+++ src/ie/tudublin/Colours.java
@@ -3,20 +3,28 @@ package ie.tudublin;
 import java.util.ArrayList;
 
 import processing.core.PApplet;
-
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Colours extends PApplet {
 
+    public int r, g, b;
+    public int value;
+    private String colour;
 
-    public Colours(){
-
+    public Colours(int _r, int _g, int _b, int _value, String _colour){
+        this.r = _r;
+        this.g = _g;
+        this.b = _b;
+        this.value = _value;
+        this.colour = _colour;
     }
 
     public Colours(ArrayList<Colours> a){
-        
+        a = new ArrayList<>();
     }
     
-    public void localColours(){
+    public void loadColours(Table t){
 
     }
     
